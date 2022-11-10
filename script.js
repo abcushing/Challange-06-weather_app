81e96cf985b703b62089869a40fde34215f02c85
abcushing <abcushing789@gmail.com>
2022-11-10 16:16:04 -0700

    5th-commit
    5th commit almost have the dates done

--- script.js
+++ script.js
@@ -1,19 +1,30 @@
 const APIKEY = 'addd896774748bd74c75435e0840431b';
 const TODAY = new Date().toLocaleDateString();
+var cityArray = [];
 
-
-// get locaion api
+// get locaion from Location API
 function getLatLon(search) {
     fetch('http://api.openweathermap.org/geo/1.0/direct?q=' + search + '&appid=' + APIKEY)
         .then((response) => response.json())
         .then((data) => {
             console.log(TODAY)
             console.log(data[0])
+            // capitalize the city if not capitalized in search
             var city = search.toLowerCase();
             var cityName = city.replace(city[0], city[0].toUpperCase());
             $('#currentCity').html(cityName);
             $('#currentDate').html(TODAY);
-            localStorage.setItem('city', cityName);
+            console.log('cityArray' + cityArray + 'includes');
+            if (cityArray.includes(cityName)) {
+                console.log('message')
+            } else {
+
+                cityArray.push(cityName);
+                console.log('something else' + cityArray);
+
+            }
+            // add to local storage
+            localStorage.setItem('city', JSON.stringify(cityArray));
             var location = {
                 lat: data[0].lat,
                 lon: data[0].lon
@@ -23,38 +34,63 @@ function getLatLon(search) {
         })
 
 }
-// get weather api
+// get weather from Weather API
 function getWeather(lat, lon) {
     fetch('https://api.openweathermap.org/data/3.0/onecall?lat=' + lat + '&lon=' + lon + '&appid=' + APIKEY)
         .then((response) => response.json())
         .then((data) => {
-            console.log('i got this');
             console.log(data.daily[0]);
+            // temp is in Kelvin, needs to be converted to Ferinheight
             let tempF = ((data.current.temp - 273.15) * 1.8) + 32;
             let formatTemp = Math.round(tempF * 100) / 100;
+            // current temp (todays weather)
             $('#currentTemp').append(formatTemp);
             $('#currentWind').append(data.current.wind_speed);
             $('#currentHumidity').append(data.current.humidity);
+            // 6 day forcast, looks better than the 5 day version
+            console.log(data + "daily temps");
             for (i = 0; i < 6; i++) {
-                console.log(data.daily[i].temp);
+             
                 let dailyTempF = ((data.daily[i].temp.max - 273.15) * 1.8) + 32;
                 let formatDailyTemp = Math.round(dailyTempF * 100) / 100;
+                let forcast = new Date(data.daily[i].dt).toLocaleDateString();
                 $('#' + i + 'Temp').append(formatDailyTemp);
                 $('#' + i + 'Wind').append(data.daily[i].wind_speed);
                 $('#' + i + 'Humidity').append(data.daily[i].humidity);
+                $('#' + i + 'Date').append(forcast);
             }
         })
 
 }
 
-
-
+// get cities from local storage
 function retrieveLocalStorage() {
-
+    var myCitys = localStorage.getItem('city');
+    let cityArray = myCitys.split(',');
+    console.log('myCitys' + typeof (cityArray));
+    for (i = 0; i < cityArray.length; i++) {
+        console.log(cityArray[i]);
+        var cityButton = '<button id="' + cityArray[i] + '" type="button" class="btn btn-secondary">' + cityArray[i] + '</button></br></br>'
+        $('#cityButtons').append(cityButton);
+    }
 }
 
 // // main process
+
 $(document).ready(function () {
+    // window.onload = function () {
+        retrieveLocalStorage();
+        console.log(cityArray + "onload");
+    // }
+
+    // retrieveLocalStorage();
+    for (i = 0; i < cityArray.length; i++) {
+    $("#" + cityArray[i]).onclick = function () {
+        city = cityArray[i];
+        console.log(city);
+        getLatLon(city);
+    }
+    }
     console.log($(citySearch));
     var city;
     submitCity.onclick = function () {
